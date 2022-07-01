37ede605506f888e7020893df4094e49b44d0739
ani-laliashvili <71911564+ani-laliashvili@users.noreply.github.com>
2022-07-01 13:17:43 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,20 +3,17 @@ import requests
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
-## GET SONGS ###
+CHART_NAME = 'hot-100' 
+
+############# GET SONGS ################
 date = ''
-#date = '2022-04-12'  ## Test
 year = date.split('-')[0]
 while len(date.split('-')) != 3 and len(date) != 10:
     date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD:")
 
-CHART_NAME = 'hot-100'
-CHART_NAME = 'germany-songs-hotw'
 PLAYLIST_NAME = f'{date} Billboard 100'
-PLAYLIST_NAME = f'{date} Billboard GERMANY'
 
-#response = requests.get(f"https://www.billboard.com/charts/hot-100/{date}/")
-response = requests.get(f"https://www.billboard.com/charts/{CHART_NAME}/{date}/")
+response = requests.get(f"https://www.billboard.com/charts/hot-100/{date}/")
 
 website = response.text
 soup = BeautifulSoup(website, 'html.parser')
@@ -24,29 +21,27 @@ titles = soup.select('li ul li h3#title-of-a-story')
 
 song_titles = [title.getText().strip() for title in titles]
 
-## SPOTIFY AUTH ##
+############ SPOTIFY AUTH #############
 OAUTH_AUTHORIZE_URL= 'https://accounts.spotify.com/authorize'
 sp = spotipy.Spotify(
     auth_manager=SpotifyOAuth(
         scope="playlist-modify-private",
         redirect_uri="https://example.com/",
-        client_id="f7d8d10fed0e41b38c91097e2721e9aa",
-        client_secret="49a0b1a9a0eb414db59b4bdded83ed89",
+        client_id="MYCLIENTID",
+        client_secret="MYCLIENTSECRET",
         show_dialog=True,
         cache_path="token.txt"
     )
 )
 
+###### check if playlist already exists     ########
 user_id = sp.current_user()['id']
-
-# check if playlist already exists
 playlists = sp.user_playlists(user=user_id)['items']
 playlist_name_found = False
 for playlist in playlists:
     if playlist['name'] == PLAYLIST_NAME:
         playlist_name_found = True
         playlist_id = playlist['id']
-
 
 if not playlist_name_found:
     playlist = sp.user_playlist_create(user=user_id, name=PLAYLIST_NAME, public=False, collaborative=False)
@@ -55,8 +50,9 @@ else:
     print("The playlist already exists.")
     quit()
 
-uri_list = []
 
+########### ADD SONGS ##########
+uri_list = []
 for track in song_titles:
     query = f'track:{track} year:{year}'
     artist = sp.search(q=query, limit = 1, type='track')
